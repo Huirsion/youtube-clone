aa6e2d9b9803e1b1678981b04407716bfa99979d
Huir Choi <youngbibs14@naver.com>
2022-12-22 17:14:01 +0900

    Use External Middleware(morgan)

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
 
-const logger = (req, res, next) => {   
-    console.log(`method : ${req.method}, url : ${req.url}`);
-    next();
-}
+const logger = morgan("dev");
 
 const controllerHome = (req, res) => {
     return res.send("Hello");
 }
 
+const controllerLogin = (req, res) => {
+    return res.send("login");
+}
+
 app.use(logger);
 app.get("/", controllerHome);
+app.get("/login", controllerLogin);
 
 
 const handleListning = () => 
