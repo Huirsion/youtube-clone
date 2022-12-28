61595a661ac7f482a586c77f38dd40188c0f42ed
Huir Choi <youngbibs14@naver.com>
2022-12-28 23:56:23 +0900

    pug mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,31 @@
-const fakeUser = {
-    username : "Huir",
-    loggedIn : false,
-}
-
 export const trending = (req, res) => {
-    return res.render("home", {pageTitle : "Home", fakeUser: fakeUser});
+    const videos = [
+        {
+            title : "Vlog #1",
+            rating : 5,
+            comments : 2,
+            createdAt : "2 minutes ago",
+            views : 59,
+            id : 1,
+        },
+        {
+            title : "Vlog #2",
+            rating : 5,
+            comments : 1000,
+            createdAt : "43 minutes ago",
+            views : 7648,
+            id : 2,
+        },
+        {
+            title : "Vlog #3",
+            rating : 3,
+            comments : 23451,
+            createdAt : "10 minutes ago",
+            views : 23514235,
+            id : 3,
+        },
+    ];
+    return res.render("home", {pageTitle : "Home", videos});
 }
 
 export const seeVideo = (req, res) => {
