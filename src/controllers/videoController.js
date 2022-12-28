dad4ca037eedc74e79a5cb0d0b2caa236b4def3f
Huir Choi <youngbibs14@naver.com>
2022-12-29 01:32:17 +0900

    array database #1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,35 +1,39 @@
+let videos = [
+    {
+        title : "Vlog #1",
+        rating : 5,
+        comments : 2,
+        createdAt : "2 minutes ago",
+        views : 59,
+        id : 1,
+    },
+    {
+        title : "Vlog #2",
+        rating : 5,
+        comments : 1000,
+        createdAt : "43 minutes ago",
+        views : 7648,
+        id : 2,
+    },
+    {
+        title : "Vlog #3",
+        rating : 3,
+        comments : 23451,
+        createdAt : "10 minutes ago",
+        views : 23514235,
+        id : 3,
+    },
+];
+
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title : "Vlog #1",
-            rating : 5,
-            comments : 2,
-            createdAt : "2 minutes ago",
-            views : 59,
-            id : 1,
-        },
-        {
-            title : "Vlog #2",
-            rating : 5,
-            comments : 1000,
-            createdAt : "43 minutes ago",
-            views : 7648,
-            id : 2,
-        },
-        {
-            title : "Vlog #3",
-            rating : 3,
-            comments : 23451,
-            createdAt : "10 minutes ago",
-            views : 23514235,
-            id : 3,
-        },
-    ];
     return res.render("home", {pageTitle : "Home", videos});
 }
 
 export const seeVideo = (req, res) => {
-    return res.render("watch", {pageTitle : "Watch"});
+    // const id = req.params.id;
+    const { id } = req.params; // ES6
+    const video = videos[id-1];
+    return res.render("watch", {pageTitle : `Watching ${video.title}`});
 }
 
 export const editVideo = (req, res) => {
