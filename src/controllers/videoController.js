57164fa03356314e01cf18f039456684f7e3535f
Huir Choi <youngbibs14@naver.com>
2022-12-30 03:03:05 +0900

    upload video(with array,,,)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -55,6 +55,16 @@ export const getUpload = (req, res) => {
 }
 
 export const postUpload = (req, res) => {
-    // video array 추가 예정
+    const { title } = req.body;
+    const newVideo = {
+        // title : req.body.title,
+        title : title,
+        rating : 0,
+        comments : 0,
+        createdAt : "Just now",
+        views : 0,
+        id : videos.length + 1,
+    }
+    videos.push(newVideo);
     return res.redirect("/");
 }
