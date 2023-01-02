8adefc075a986147cb9abbdd186c95144ece64b9
Huir Choi <youngbibs14@naver.com>
2023-01-03 02:41:08 +0900

    try catch문으로 에러상황 처리, schema에서 타입 구체화

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,7 @@ import Video from "../models/Video";
 export const home = async(req, res) => {
     const videos = await Video.find();
     console.log(videos);
-    return res.render("home", {pageTitle : "Home", videos : []});
+    return res.render("home", {pageTitle : "Home", videos});
 }
 
 export const watchVideo = (req, res) => {
@@ -30,18 +30,16 @@ export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle : "Upload Video" });
 }
 
-export const postUpload = (req, res) => {
-    const { title, description, hashtags } = req.body;
-    const video = new Video({
-        title, // title : title,
-        description, // description : description,
-        createdAt: Date.now(),
-        hashtags: hashtags.split(",").map((word) => `#${word}`),
-        meta: {
-            views: 0,
-            rating: 0,
-        },
-    });
-    console.log("Video : ", video);
-    return res.redirect("/");
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;   
+    try{
+        await Video.create({
+            title, // title : title,
+            description, // description : description,
+            hashtags: hashtags.split(",").map((word) => `#${word}`),
+        });
+        return res.redirect("/");
+    } catch(error){
+        return res.render("upload", { pageTitle : "Upload Video", errorMessage: error._message });
+    }
 }
