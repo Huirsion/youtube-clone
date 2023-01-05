20748e33edfda0e1f749c8d0b887aa0221fe6fce
Huir Choi <youngbibs14@naver.com>
2023-01-06 00:07:55 +0900

    mongoose middleware를 사용해서 creat/update 전에 데이터 체크하기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,7 +36,7 @@ export const postEdit = async (req, res) => {
     await Video.findByIdAndUpdate(id, {
         title : title,
         description : description,
-        hashtags: hashtags.split(",").map((word) => word.startsWith("#") ? word : `#${word}`),
+        hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect(`/videos/${id}`);
 }
@@ -51,7 +51,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title, // title : title,
             description, // description : description,
-            hashtags: hashtags.split(",").map((word) => word.startsWith("#") ? word : `#${word}`),
+            hashtags : Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
     } catch(error){
