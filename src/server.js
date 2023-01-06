e5e61c8a7534e056742ff45ef3cfde055c89f6a5
Huir Choi <youngbibs14@naver.com>
2023-01-07 04:39:06 +0900

    session id를 이용해 유저 구분하기

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import session from "express-session";
 import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -12,6 +13,25 @@ app.set("views", process.cwd() + "/src" + "/views");
 
 app.use(logger);
 app.use(express.urlencoded({extended : true}));
+app.use(
+    session({
+        secret : "Hello",
+        resave : true,
+        saveUninitialized : true,
+    })
+);
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    });
+});
+
+app.get("/add-one", (req, res, next) => {
+    req.session.counter += 1;
+    return res.send(`${req.session.id}\n${req.session.counter}`);
+});
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
