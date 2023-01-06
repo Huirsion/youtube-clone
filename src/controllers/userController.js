a67faf57e3a4233b6515163947d10f838b145a67
Huir Choi <youngbibs14@naver.com>
2023-01-07 00:23:13 +0900

    create account and hashing password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,19 @@
-export const join = (req, res) => {
-    res.send("User Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
+    return res.render("join", { pageTitle : "Sign In" });
+}
+
+export const postJoin = async (req, res) => {
+    const { name, username, email, password, location } = req.body;
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+    return res.redirect("/login");
 }
 
 export const editUser = (req, res) => {
